46bef41eac21e61d55199b12907469c3a2fb42cb
Nicolas VINCENT <nico.vince@gmail.com>
2023-07-19 19:04:51 +0200

    Starter: generation de l'entrée de la recette
    Ajout du lien vers la recette en bas du README

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-    echo "Usage:"
+    echo "Utilisation:"
     echo "$0 <prefixe nom de fichier> <nom de la recette>"
     echo "  Exemple: $0 gateau_chocoloat 'Gateau au Chocolat'"
     exit 1
@@ -11,3 +11,5 @@ RECIPE_FILENAME="${1}.md"
 RECIPE_NAME="${2}"
 cat template.md | sed "s/^# Nom du plat/# ${RECIPE_NAME}/" > "${RECIPE_FILENAME}"
 git add ${RECIPE_FILENAME}
+echo "- [${RECIPE_NAME}](${RECIPE_FILENAME})" >> README.md
+echo "Editer README.md pour mettre la recette dans la bonne section."
